c5736a926d9a5bf341a9717ddc0790c19a16426b
Laambert <98312235+Laambert@users.noreply.github.com>
2023-01-02 22:30:40 +0100

    Add files via upload

--- app.js
+++ app.js
@@ -1,14 +1,18 @@
+const removeBut = document.querySelectorAll(".removeBut");
+const checkButtons = document.querySelectorAll(".checkBox");
+const navPages = document.querySelectorAll(".nav-item a");
+const saves = document.querySelectorAll(".box");
+const activeNav = document.querySelector(".active-nav");
 const startBut = document.getElementsByClassName("buttons")[0];
 const stopBut = document.getElementsByClassName("buttons")[1];
 const mainBlock = document.getElementsByClassName("mainBlock")[0];
 const timer = document.getElementsByClassName("timer")[0];
-const checkButtons = document.querySelectorAll(".checkBox");
-const navPages = document.querySelectorAll(".nav-item a");
-const activeNav = document.querySelector(".active-nav");
 const saveBut = document.getElementsByClassName("fa-save")[0];
 const savesBlock = document.getElementsByClassName("savesBlock")[0];
 const nav = document.getElementsByTagName("nav")[0];
 const node = document.getElementsByClassName("timesave")[0];
+
+
 gsap.registerPlugin(Flip);
 let upperChecked = true;
 let interval = null;
@@ -44,32 +48,33 @@ navPages.forEach(page => {
             ease: "elastic.out(1,0.5)",
         })
     })
-
-
 })
 
+function startInterval() {
+    interval = setInterval(function() {
+        count[1] += 1
+
+        time[2] = ('0' + count[1]).slice(-2)
+        if (time[2] == 60) {
+            count[1] = 0
+            count[0] += 1
+            time[0] = ('0' + count[0]).slice(-2)
+            time[2] = ('0' + count[1]).slice(-2)
+
+        } else {
+
+        }
+        timer.textContent = time[0] + time[1] + time[2] + units
+        console.log(time)
+    }, speed)
+}
 
 
 function startTimer() {
 
     if (interval === null) {
         gsap.to(saveBut, { color: "transparent" });
-        interval = setInterval(function() {
-            count[1] += 1
-
-            time[2] = ('0' + count[1]).slice(-2)
-            if (time[2] == 60) {
-                count[1] = 0
-                count[0] += 1
-                time[0] = ('0' + count[0]).slice(-2)
-                time[2] = ('0' + count[1]).slice(-2)
-
-            } else {
-
-            }
-            timer.textContent = time[0] + time[1] + time[2] + units
-            console.log(time)
-        }, speed)
+        startInterval()
     } else {
 
     }
@@ -84,7 +89,10 @@ function buttonChecked() {
         document.getElementById("Ms").style.borderColor = "#708090"
         units = "s"
         timer.textContent = time[0] + time[1] + time[2] + units
+        clearInterval(interval)
         speed = 1000
+        startInterval()
+
     } else {
         upperChecked = false;
         document.getElementById("Ms").style.borderColor = "#385ae0"
@@ -93,7 +101,9 @@ function buttonChecked() {
         document.getElementById("S").style.borderColor = "#708090"
         units = "ms"
         timer.textContent = time[0] + time[1] + time[2] + units
+        clearInterval(interval)
         speed = 100
+        startInterval()
     }
 
 }
@@ -124,7 +134,15 @@ function addTimeToBlock() {
         removed = true
         document.getElementsByClassName("savesBlock")[0].removeChild(scnd)[1]
     }
-    console.log(time)
+    saves.forEach(save => {
+        gsap.to(saves, { color: "black" });
+    })
+    removeBut.forEach(but => {
+        gsap.to(removeBut, { borderColor: "gray" });
+    })
+
+
+
 }
 
 function remove(el) {
